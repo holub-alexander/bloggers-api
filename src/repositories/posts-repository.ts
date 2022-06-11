1c73e67bc6c0a0871d71e160eeeefa4fdfad8c7e
Alexander Holub <holub.alexander.it@gmail.com>
2022-06-11 16:02:30 +0300

    feat: set up POST method for posts, set up validations

--- src/repositories/posts-repository.ts
+++ src/repositories/posts-repository.ts
@@ -1,4 +1,5 @@
 import { IPost, POSTS } from '../data/posts-data';
+import { IFieldError } from '../types/field-error';
 
 const postsRepository = {
   getAllPosts: (): IPost[] => {
@@ -9,6 +10,27 @@ const postsRepository = {
     const findPost = POSTS.find((post) => post.id.toString() === id);
 
     return findPost || null;
+  },
+
+  addPost: (
+    title: string,
+    shortDescription: string,
+    content: string,
+    bloggerId: number,
+    bloggerName: string
+  ): IPost | IFieldError => {
+    const newPost: IPost = {
+      id: new Date().valueOf(),
+      title,
+      shortDescription,
+      content,
+      bloggerId,
+      bloggerName,
+    };
+
+    POSTS.push(newPost);
+
+    return newPost;
   },
 };
 
--- src/routes/posts-route.ts
+++ src/routes/posts-route.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+import bloggersRepository from '../repositories/bloggers-repository';
 import postsRepository from '../repositories/posts-repository';
+import errorsOccured from '../utils/errors-occured';
+import postValidationSchema from '../utils/validations/post-validation-schema.ts';
 
 const postsRoute = Router();
 
@@ -12,6 +16,36 @@ postsRoute.get('/posts/:id', (req: Request, res: Response) => {
 
   if (post) {
     res.send(post);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+postsRoute.post('/posts', postValidationSchema, (req: Request, res: Response) => {
+  const errors = errorsOccured(validationResult(req));
+  const errorsMessages = errors.errorsMessages;
+  const blogger = bloggersRepository.getBloggerById(req.body.bloggerId.toString());
+
+  if (!blogger) {
+    errorsMessages.push({ message: 'Blogger not found', field: 'bloggerName' });
+  }
+
+  if (errorsMessages.length > 0) {
+    res.status(400).send(errors);
+
+    return;
+  }
+
+  const post = postsRepository.addPost(
+    req.body.title,
+    req.body.shortDescription,
+    req.body.content,
+    +req.body.bloggerId,
+    blogger!.name
+  );
+
+  if (post) {
+    res.status(201).send(post);
   } else {
     res.sendStatus(404);
   }
